3cbb41e559c4305f80587078d1970ca814c4f53c
Aapo Laakkio <aapolaakkio@outlook.com>
2025-05-13 20:37:37 +0300

    Add robots.txt

---
+++ src/app/robots.ts
@@ -0,0 +1,16 @@
+import type { MetadataRoute } from 'next'
+
+import { env } from '@/env'
+
+export default function robots(): MetadataRoute.Robots {
+  const serverURL = env.NEXT_PUBLIC_SERVER_URL
+
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/admin']
+    },
+    sitemap: `${serverURL}/sitemap.xml`
+  }
+}
