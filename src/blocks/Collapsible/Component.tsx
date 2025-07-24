a1e4c436db3310c0d11a35f9a14804ddefc3cab7
Aapo Laakkio <aapolaakkio@outlook.com>
2025-07-24 17:35:50 +0300

    Fix collapsible styles

--- src/blocks/Collapsible/Component.tsx
+++ src/blocks/Collapsible/Component.tsx
@@ -12,27 +12,28 @@ interface CollapsibleBlockProps {
 }
 
 export const Collapsible = ({ block, locale }: CollapsibleBlockProps) => {
-  const contentId = `collapsible-content-${Math.random().toString(36).substr(2, 9)}`
+  const contentId = `collapsible-content-${block.id ?? Math.random().toString(36).substring(2, 9)}`
 
   return (
-    <div className="prose-h2:my-3 w-full">
-      <details open={block.isOpenByDefault ?? false} className="group rounded">
-        <summary
-          className="border-fk-gray flex w-full cursor-pointer list-none items-center justify-between rounded border-2 p-3 [&::-webkit-details-marker]:hidden"
-          aria-controls={contentId}
-        >
-          <div className="text-left text-lg font-bold">{block.title}</div>
-          <div className="flex h-7 w-7 items-center justify-center">
-            <ChevronDown
-              size={24}
-              className="transition-transform duration-200 group-open:rotate-180"
-            />
-          </div>
-        </summary>
-        <div id={contentId} className="px-4 pb-3">
-          <RichText data={block.content} locale={locale} />
+    <details
+      open={block.isOpenByDefault ?? false}
+      className="group border-fk-gray m-3 w-full rounded border-2"
+    >
+      <summary
+        className="border-fk-gray flex w-full cursor-pointer list-none items-center justify-between rounded p-3 group-open:border-b-2 [&::-webkit-details-marker]:hidden"
+        aria-controls={contentId}
+      >
+        <div className="text-left text-lg font-bold">{block.title}</div>
+        <div className="flex h-7 w-7 items-center justify-center">
+          <ChevronDown
+            size={24}
+            className="transition-transform duration-200 group-open:rotate-180"
+          />
         </div>
-      </details>
-    </div>
+      </summary>
+      <div id={contentId} className="px-4 pb-3">
+        <RichText data={block.content} locale={locale} />
+      </div>
+    </details>
   )
 }
