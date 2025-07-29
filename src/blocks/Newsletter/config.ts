95cb7880cb1286b910677645c5e80509e3942233
Aapo Laakkio <aapolaakkio@outlook.com>
2025-07-29 22:30:13 +0300

    Delete unnecessary fields from newsletter block

--- src/blocks/Newsletter/config.ts
+++ src/blocks/Newsletter/config.ts
@@ -1,4 +1,3 @@
-import { BlocksFeature, HeadingFeature, lexicalEditor } from '@payloadcms/richtext-lexical'
 import type { Block } from 'payload'
 
 export const NewsletterBlock: Block = {
@@ -10,36 +9,6 @@ export const NewsletterBlock: Block = {
       type: 'relationship',
       relationTo: 'newsletters',
       required: true
-    },
-    {
-      name: 'logo',
-      type: 'upload',
-      relationTo: 'media',
-      required: true,
-      admin: {
-        description: 'Logo shown in the top right corner of the weekly news'
-      }
-    },
-    {
-      name: 'footer',
-      type: 'richText',
-      localized: true,
-      required: true,
-      editor: lexicalEditor({
-        features: ({ rootFeatures }) => {
-          return [
-            ...rootFeatures,
-            HeadingFeature({ enabledHeadingSizes: [] }),
-            BlocksFeature({
-              blocks: [],
-              inlineBlocks: []
-            })
-          ]
-        }
-      }),
-      admin: {
-        description: 'Footer text shown at the bottom of the weekly news'
-      }
     }
   ]
 }
--- src/payload-types.ts
+++ src/payload-types.ts
@@ -690,28 +690,6 @@ export interface IconBlock {
  */
 export interface NewsletterBlock {
   newsletter: number | Newsletter;
-  /**
-   * Logo shown in the top right corner of the weekly news
-   */
-  logo: number | Media;
-  /**
-   * Footer text shown at the bottom of the weekly news
-   */
-  footer: {
-    root: {
-      type: string;
-      children: {
-        type: string;
-        version: number;
-        [k: string]: unknown;
-      }[];
-      direction: ('ltr' | 'rtl') | null;
-      format: 'left' | 'start' | 'center' | 'right' | 'end' | 'justify' | '';
-      indent: number;
-      version: number;
-    };
-    [k: string]: unknown;
-  };
   id?: string | null;
   blockName?: string | null;
   blockType: 'newsletter';
