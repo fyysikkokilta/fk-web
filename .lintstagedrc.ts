2b9af05e050b822f76b6ebaf3afc2c302c060fa6
Aapo Laakkio <aapolaakkio@outlook.com>
2025-08-20 19:03:22 +0300

    Use ts lintstagedr

--- .lintstagedrc.ts
+++ .lintstagedrc.ts
@@ -1,7 +1,7 @@
 import path from 'path'
 
-const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`
+const buildEslintCommand = (filenames: string[]) =>
+  `eslint --fix ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')}`
 
 const lintStagedConfig = {
   '**/*.ts?(x)': () => 'tsc --noEmit',
